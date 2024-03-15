6a7aa3278f694563ea9b7b4836857dd523935bf0
juliacheng37 <juliacheng37@gmail.com>
2024-03-15 11:26:56 +0000

    New changes

--- python_files/distance_airports.py
+++ python_files/distance_airports.py
@@ -1,7 +1,7 @@
 from geopy.distance import geodesic
 
 def distance_geo(latitude_source, longitude_source, latitude_destination, longitude_destination):
-    """
+    """ 
     Calculates the geodesic distance in kilometers between two geographical points.
 
     Args:
@@ -13,7 +13,7 @@ def distance_geo(latitude_source, longitude_source, latitude_destination, longit
     Returns:
     - float: Distance between the two points in kilometers. Returns 0 in case of error.
     """
-    
+
     try:
         geoval = geodesic((latitude_source, longitude_source), (latitude_destination, longitude_destination)).km
     except:
