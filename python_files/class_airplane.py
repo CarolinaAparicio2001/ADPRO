137b7c2f2bd536aa4cb4534599212baa57b77216
juliacheng37 <juliacheng37@gmail.com>
2024-03-15 17:52:56 +0000

    adding last details

--- python_files/class_airplane.py
+++ python_files/class_airplane.py
@@ -149,6 +149,13 @@ class Airplane:
         return self.merge_df
 
     def plot_airports_in_country(self, country):
+        """
+        Plot the locations of airports within a specified country on a map.
+
+        Parameters
+        - country(str): The name of the country for which airports are to be plotted.
+        
+        """
         country_airports = self.merge_df[self.merge_df["Source country"] == country]
         if country_airports.empty:
             print(f"No airports found in {country}.")
